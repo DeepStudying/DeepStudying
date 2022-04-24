fb320021556d955e9ac1594fff87eeef9adc2fcf
joaovsz <joaovitor1713coin@gmail.com>
2022-04-24 03:03:49 -0300

    login preview

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <main>
+        <div className="container">
+            <form>
+                
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
